d8029793649d483edcc8973f92483fa2a708d95e
Anna Popova <anna.popova201909@gmail.com>
2021-09-07 17:48:05 +0300

    added tests

--- src/main/java/ru/otus/SpringMvcHomework/controllers/BooksController.java
+++ src/main/java/ru/otus/SpringMvcHomework/controllers/BooksController.java
@@ -4,68 +4,66 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.otus.SpringMvcHomework.dao.AuthorDao;
-import ru.otus.SpringMvcHomework.dao.BookDao;
-import ru.otus.SpringMvcHomework.dao.GenreDao;
 import ru.otus.SpringMvcHomework.domain.Book;
+import ru.otus.SpringMvcHomework.service.BookService;
 
 @Controller
 @AllArgsConstructor
 @RequestMapping("/books")
 public class BooksController {
 
-    private final BookDao bookDao;
-    private final AuthorDao authorDao;
-    private final GenreDao genreDao;
+    private final BookService bookService;
+//    private final AuthorDao authorDao;
+//    private final GenreDao genreDao;
 
     @GetMapping("/getById")
     public String getById(@RequestParam("id") Long id, Model model) {
-        model.addAttribute("booksById", bookDao.getById(id));
+        model.addAttribute("booksById", bookService.findBookById(id));
         return "books/getById";
     }
 
     @GetMapping("/getByName")
     public String getByName(@RequestParam("name") String bookName, Model model) {
-        model.addAttribute("booksByName", bookDao.getByName(bookName));
+        model.addAttribute("booksByName", bookService.findBookByName(bookName));
         return "books/getByName";
     }
 
     @GetMapping("/getByGenre")
     public String getByGenre(@RequestParam("genre") String genre, Model model) {
-        model.addAttribute("booksByGenre", bookDao.getByGenreName(genre));
+        model.addAttribute("booksByGenre", bookService.findBookByGenre(genre));
         return "books/getByGenre";
     }
 
     @GetMapping("/getByAuthor")
     public String getByAuthor(@RequestParam("author") String author, Model model) {
-        model.addAttribute("booksByAuthor", bookDao.getByAuthorName(author));
+        model.addAttribute("booksByAuthor", bookService.findBookByAuthor(author));
         return "books/getByAuthor";
     }
 
     @GetMapping()
     public String showAll(Model model) {
-        model.addAttribute("books", bookDao.findAll());
+        model.addAttribute("books", bookService.showAllBooks());
         return "books/showAll";
     }
 
-    @GetMapping("/newBook")
-    public String newBook(Model model) {
-        model.addAttribute("book", new Book());
-        model.addAttribute("authors", authorDao.findAll());
-        model.addAttribute("genres", genreDao.findAll());
-        return "books/new";
-    }
+//    @GetMapping("/newBook")
+//    public String newBook(Model model) {
+//        model.addAttribute("book", new Book());
+//        model.addAttribute("authors", authorDao.findAll());
+//        model.addAttribute("genres", genreDao.findAll());
+//        return "books/new";
+//    }
 
     @PostMapping()
     public String create(@ModelAttribute("book") Book book) {
-        bookDao.save(book);
+        bookService.createBook(book);
         return "books/success";
     }
 
     @ResponseBody
     @DeleteMapping("/delete")
     public void delete(@RequestParam(name = "id") long id) {
-        bookDao.deleteById(id);
+        bookService.deleteBookById(id);
     }
 }
 
---
+++ src/test/java/ru/otus/SpringMvcHomework/rest/BooksControllerTest.java
@@ -0,0 +1,86 @@
+package ru.otus.SpringMvcHomework.rest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.otus.SpringMvcHomework.controllers.BooksController;
+import ru.otus.SpringMvcHomework.domain.Author;
+import ru.otus.SpringMvcHomework.domain.Book;
+import ru.otus.SpringMvcHomework.domain.Genre;
+import ru.otus.SpringMvcHomework.service.BookService;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(BooksController.class)
+public class BooksControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+//    @MockBean
+//    private BookDao bookDao;
+//    @MockBean
+//    private GenreDao genreDao;
+
+    @BeforeEach
+    void init() {
+        Author author = new Author((long) 1, "Blok");
+        Genre genre = new Genre((long) 1, "Poetry");
+        Book book = new Book((long) 1, "The sun", author, genre, null);
+        Mockito.when(bookService.showAllBooks()).thenReturn(List.of(book));
+
+    }
+
+
+    @WithMockUser(
+            username = "admin",
+            password = "adminPass",
+            roles = "ADMIN"
+    )
+
+    @Test
+    public void testAuthenticatedOnAdmin() throws Exception {
+
+        mockMvc.perform(get("/books"))
+                .andExpect(status().isOk());
+
+    }
+
+    @WithMockUser(
+            username = "user",
+            password = "userPa",
+            roles = "USER"
+    )
+
+    @Test
+    public void testAuthenticatedOnUserGettingBookByAuthor() throws Exception {
+        mockMvc.perform(get("/getByAuthor"))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @WithMockUser(
+            username = "user",
+            password = "userPa",
+            roles = "USER"
+    )
+
+    @Test
+    public void testAuthenticatedOnUserGettingBookByGenre() throws Exception {
+        mockMvc.perform(get("/getByGenre"))
+                .andExpect(status().is4xxClientError());
+    }
+
+
+}
